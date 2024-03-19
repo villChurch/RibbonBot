fc6ee770619568b0463bbe7d363906c4e597a424
Will Spires <williamspires@hotmail.co.uk>
2024-03-19 22:10:57 +0000

    Add after dark option to movie addition and movie table

--- RibbonBotDAL/Data/IMovieData.cs
+++ RibbonBotDAL/Data/IMovieData.cs
@@ -10,6 +10,8 @@ namespace RibbonBotDAL.Data
 
         Task AddMovie(string movie);
 
+        Task AddMovie(string movie, bool afterDark);
+
         Task<bool> UpdateMovie(Movies movie);
     }
 }
